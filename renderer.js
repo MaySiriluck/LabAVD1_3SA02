bb78306b90e3878c1052c166e97c2e9554774db0
MaySiriluck <54760051+MaySiriluck@users.noreply.github.com>
2019-09-01 12:12:08 +0700

    Add files via upload

--- renderer.js
+++ renderer.js
@@ -1,3 +1,33 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+express = opeRand = (ops) => {
+    var a = document.getElementById("num1").value;
+    var b = document.getElementById("num2").value;
+    var sum = 0;
+    if (ops == "Sum")
+        {
+            sum = Number(a)+Number(b);
+            document.getElementById("add").innerHTML="+";
+            alert('Answer: ' +sum);
+        }
+    else if (ops == "Min")
+        {
+            sum = Number(a)-Number(b);
+            document.getElementById("add").innerHTML="-";
+            alert('Answer: ' +sum); 
+        }
+    else if (ops == "Mul")
+        {
+            sum = Number(a)*Number(b);
+            document.getElementById("add").innerHTML="x";
+            alert('Answer: ' +sum);
+        }
+    else if (ops == "Div")
+        {
+            sum = Number(a)/Number(b);
+            document.getElementById("add").innerHTML="/";
+            alert('Answer: ' +sum);
+        }
+    }
